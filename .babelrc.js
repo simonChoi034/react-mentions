f784226cd0e783601a7c060f50f52a11627594b3
simonChoi034 <simonchoi034@gmail.com>
2023-03-28 14:24:03 +0800

    Add index.d.ts file

--- .babelrc.js
+++ .babelrc.js
@@ -1,22 +1,28 @@
-const { NODE_ENV } = process.env
+const {NODE_ENV} = process.env
 
 module.exports = {
-  presets: [
-    [
-      '@babel/env',
-      {
-        modules: NODE_ENV === 'test' ? 'auto' : false,
-      },
+    presets: [
+        [
+            '@babel/env',
+            {
+                modules: NODE_ENV === 'test' ? 'auto' : false,
+            },
+        ],
+        '@babel/preset-flow',
+        '@babel/preset-react',
+        '@babel/preset-typescript'
     ],
-    '@babel/react',
-    '@babel/preset-flow',
-  ],
-  comments: true,
-  plugins: [
-    '@babel/transform-runtime',
-    '@babel/plugin-transform-flow-strip-types',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-class-properties',
-  ],
+    comments: true,
+    plugins: [
+        '@babel/transform-runtime',
+        '@babel/plugin-transform-flow-strip-types',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-export-namespace-from',
+        '@babel/plugin-proposal-class-properties',
+    ],
+    include: [
+        "./@types/index.d.ts",
+        "./src/**/*",
+        "./node_modules/@types/**/*"
+    ],
 }
